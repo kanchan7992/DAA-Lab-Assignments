55db6a3f655978d70ca25f90bf240ddb1f2d3892
kanchan7992 <rautelakanchan123@gmail.com>
2021-07-21 21:37:04 +0530

    Week2

---
+++ Week2/Q2.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int main() {
+  int n,t,i,j,k, testcases;
+    cin>>testcases;
+    for(t=0;t<testcases;t++)
+    {
+        cout<<"INPUT : ";
+        cin>>n;
+        int a[n],f=0;
+        for(i=1;i<=n;i++)
+            cin>>a[i];
+        for(i=1;i<=n;i++)
+        {
+            for(j=i+1;j<=n;j++)
+            {
+                for(k=j+1;k<=n;k++)
+                {
+                    if(a[i]+a[j]==a[k])
+                    {
+                        cout<<"OUTPUT: "<<i<<" "<<j<<" "<<k<<endl<<endl;
+                        f=1;
+                        break;
+                    }
+            }
+        }
+     }
+    if(f==0)
+    {
+        cout<<"OUTPUT: No sequence found"<<endl<<endl;
+    }
+    }
+    return 0;
+}
---
+++ Week2/Q3.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main() {
+  int n,t,i,j, testcases;
+    cin>>testcases;
+    for(t=0;t<testcases;t++)
+    {
+        cout<<"INPUT : ";
+        cin>>n;
+        int a[n],f=0;
+        for(i=0;i<n;i++)
+            cin>>a[i];
+        int key;
+        cin>>key;
+
+        for(i=0;i<n-1;i++)
+        {
+            for(j=i+1;j<n;j++)
+            {
+                 if(abs(a[i]-a[j])==key)
+                    {
+                        f++;
+
+                    }
+            }
+        }
+
+
+        cout<<"OUTPUT: "<<f<<endl<<endl;
+  }
+    return 0;
+}
+
